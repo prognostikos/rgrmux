20c380dd2e74f03b1ab77fd319daa6f0cd072c53
Matt Rohrer <matt@prognostikos.com>
2013-01-28 11:38:20 +0100

    steal from guard/notifier/tmux

--- lib/rgrmux/rspec.rb
+++ lib/rgrmux/rspec.rb
@@ -1,56 +1,48 @@
 require 'rspec/core/formatters/progress_formatter'
 
 module Rgrmux
-
-  # Credits: @thedelchop - https://gist.github.com/4195788
+  # Credits:
+  #   @thedelchop - https://gist.github.com/4195788
+  #   https://github.com/guard/guard/blob/master/lib/guard/notifiers/tmux.rb
+  #
   class RSpec < ::RSpec::Core::Formatters::ProgressFormatter
 
     def initialize(output)
+      @tmux_color = ""
       @tmux_message = ""
-      @write_to_tmux = true
+      if ENV['TMUX']
+        @write_to_tmux = true
+        system "tmux set quiet on"
+      end
       super
     end
 
     def dump_summary(duration, example_count, failure_count, pending_count)
+      return super unless @write_to_tmux
+
       summary = pluralize(example_count, "example")
       summary << ", " << pluralize(failure_count, "failure")
       summary << ", #{pending_count} pending" if pending_count > 0
 
-      system "tmux set quiet on"
-
       if pending_count > 0
-        system "tmux set status-right-bg yellow"
+        @tmux_color = "yellow"
       elsif failure_count > 0
-        system "tmux set status-right-bg red"
+        @tmux_color = "red"
       else
-        system "tmux set status-right-bg green"
+        @tmux_color = "green"
       end
 
       @tmux_message = summary + @tmux_message
-
-      super
-    end
-
-    def dump_failure_info(example)
-      if @write_to_tmux
-        exception = example.execution_result[:exception]
-
-        @tmux_message << "  Failure/Error: #{read_failed_line(exception, example).strip} "
-
-        exception.message.each_line do |line|
-          @tmux_message << "#{line.gsub("\n","").strip} "
-        end
-
-        @write_to_tmux = false
-      end
-
-      super
     end
 
     def close
-      system "tmux set status-right-length #{@tmux_message.length + 5}"
-
-      system "tmux set status-right '#{@tmux_message.gsub('`','').gsub("'",'')}'"
+      if @write_to_tmux
+        system "tmux set status-left-bg #{@tmux_color}"
+        system "tmux set message-bg #{@tmux_color}"
+        system "tmux set display-time 3000"
+        system "tmux display-message '#{@tmux_message.gsub('`','').gsub("'",'')}'"
+        system "tmux set quiet off"
+      end
       super
     end
   end
